13e93e7193d54de8688d5718bb3685b66acb77eb
Orchid <orchidgoth@gmail.com>
2026-01-23 15:11:44 +0100

    Initial Commit

---
+++ Knitting Counter/Knitting_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knitting_CounterApp.swift
+//  Knitting Counter
+//
+//  Created by Orchid_Code on 23.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Knitting_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
